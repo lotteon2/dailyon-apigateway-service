dbd03aeec8844d6f184d3b5b751f53a8753032f1
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-10 17:52:34 +0900

    [FIX] header 에 붙이는 회원정보 수정

--- src/main/java/com/dailyon/apigatewayservice/Util/JwtUtil.java
+++ src/main/java/com/dailyon/apigatewayservice/Util/JwtUtil.java
@@ -1,57 +1,52 @@
 package com.dailyon.apigatewayservice.Util;
 
-import com.google.common.base.Function;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.spec.SecretKeySpec;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
 
 @Slf4j
 @Component
 public class JwtUtil {
 
-    @Autowired
-    private Environment environment;
+  @Autowired private Environment environment;
 
-    public Claims parse(String jwt) {
-        return Jwts.parser().setSigningKey(Keys.hmacShaKeyFor(environment.getProperty("secretKey").getBytes())).parseClaimsJws(jwt).getBody();
-    }
+  public Claims parse(String jwt) {
+    return Jwts.parser()
+        .setSigningKey(Keys.hmacShaKeyFor(environment.getProperty("secretKey").getBytes()))
+        .parseClaimsJws(jwt)
+        .getBody();
+  }
 
-    private Integer getUserId(Claims claims) {
-        return claims.get("userId", Integer.class);
-    }
+  private Long getMemberId(Claims claims) {
+    return claims.get("memberId", Long.class);
+  }
 
-    private String getUserRole(Claims claims){
-        return claims.get("userRole", String.class);
-    }
-    public void addJwtPayloadHeaders(ServerHttpRequest request, Claims claims) {
+  private String getUserRole(Claims claims) {
+    return claims.get("role", String.class);
+  }
 
-        Integer userId = getUserId(claims);
-        String userRole = getUserRole(claims);
-        request.mutate()
-                .header("Content-Type", "application/json;charset=UTF-8")
-                .header("memberId",String.valueOf(userId))
-                .header("role",String.valueOf(userRole))
-                .build();
-    }
+  public void addJwtPayloadHeaders(ServerHttpRequest request, Claims claims) {
+    Long memberId = getMemberId(claims);
+    String userRole = getUserRole(claims);
+    request
+        .mutate()
+        .header("Content-Type", "application/json;charset=UTF-8")
+        .header("memberId", String.valueOf(memberId))
+        .header("role", String.valueOf(userRole))
+        .build();
+  }
 
-    public void addJwtPayloadHeadersForNonAuthService(ServerHttpRequest request, Claims claims) {
-        Integer userId = (claims != null) ? getUserId(claims) : null;
-        request.mutate()
-                .header("Content-Type", "application/json;charset=UTF-8")
-                .header("memberId", String.valueOf(userId) == null ? null : String.valueOf(userId))
-                .build();
-    }
+  public void addJwtPayloadHeadersForNonAuthService(ServerHttpRequest request, Claims claims) {
+    Long memberId = (claims != null) ? getMemberId(claims) : null;
+    request
+        .mutate()
+        .header("Content-Type", "application/json;charset=UTF-8")
+        .header("memberId", String.valueOf(memberId) == null ? null : String.valueOf(memberId))
+        .build();
+  }
 }
